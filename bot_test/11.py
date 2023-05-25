b64870dbe03137f930f7c776f2249723aa20aa14
Anton Kochanov <129223554+st1mka48@users.noreply.github.com>
2023-05-25 21:55:51 +0300

    Delete 11.py

--- bot_test/11.py
+++
@@ -1,7 +0,0 @@
-questions_and_answers = {}
-the_end = ''
-while the_end != 'n':
-    que = input('Вопрос: ')
-    questions_and_answers[que] = input('Ответ: ')
-    the_end = input('Продолжить?' 'y/n ')
-print(questions_and_answers)
